950b5d6ac18e3225019438f31a2a0b850b339980
Jose28ramirez <jramirez1262@mymail.lausd.net>
2025-05-30 10:35:53 -0700

    init commit

---
+++ src/main/java/com/Joseanddeo/PracticeFinch.java
@@ -0,0 +1,16 @@
+package com.Joseanddeo;
+
+import com.birdbrain.Finch;
+
+public class PracticeFinch {
+    public static void main(String[] args) {
+        Finch bird = new Finch();
+
+        // ADD CODE HERE
+
+
+        
+        bird.stop();
+        bird.disconnect();
+    }
+}
