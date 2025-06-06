234e0bc005668d0a60203d1dce4b3b114ac670b6
Jose28ramirez <jramirez1262@mymail.lausd.net>
2025-06-06 09:53:08 -0700

    finished project

--- src/main/java/com/Joseanddeo/PracticeFinch.java
+++ src/main/java/com/Joseanddeo/PracticeFinch.java
@@ -7,9 +7,28 @@ public class PracticeFinch {
         Finch bird = new Finch();
 
         // ADD CODE HERE
-
-
+        // first long stretch
+        bird.setMove("F",113,60);
+        // first long stretch
+        bird.setTurn("L",90,100);
+        bird.setMove("F",32,60);
+        bird.setTurn("L",90,100);
         
+        bird.setMove("F",35,60);
+        bird.setTurn("R",90,100);
+        bird.setMove("F",56,60);
+        // turn it 92 degrees because it kept going off course
+        bird.setTurn("R",92,100);
+        //right before longest stretch 
+        bird.setMove("F",118,45);
+        bird.setTurn("R",90,100);
+        bird.setMove("F",24,60);
+        bird.setTurn("L",90,100);
+        bird.setMove("F",21,60);
+        bird.setTurn("R",90,100);
+        //makes it back into last part
+        bird.setMove("B",19,60);
+
         bird.stop();
         bird.disconnect();
     }
